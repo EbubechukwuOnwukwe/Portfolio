0f697ccf2f72528834ebac6d7c6a48b714e6724b
Ebubechukwu Onwukwe <ebube5298@gmail.com>
2026-06-17 15:13:49 +0100

    Added fade in's and out's

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,17 +8,29 @@ import Contact from './pages/Contact';
 import About from './pages/About';
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <Router>
+    <>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact/>} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact/>} />
+            </Routes>
+          </motion.div>
+        </AnimatePresence>
         <Footer />
-    </Router>
+    </>
   )
 }
 
